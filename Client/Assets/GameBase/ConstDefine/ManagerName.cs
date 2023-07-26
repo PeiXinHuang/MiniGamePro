9c1c713981167e292282db0345f3279432483f4d
PeiXinHuang <peixinhuang0428@gmail.com>
2023-07-26 22:41:51 +0800

    前端【Delete】干掉ToLua框架代码

--- Client/Assets/GameBase/ConstDefine/ManagerName.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-namespace GameBase {
-    public class ManagerName {
-        public const string Lua = "LuaManager";
-        public const string Game = "GameManager";
-    }
-}
--- Client/Assets/ToLua/Lua/GameScripts/View/Base/UIManager.lua
+++
@@ -1,6 +0,0 @@
----@class UIManager
-module("UIManager", package.seeall)
-
-function UIManager.OpenView(var)
-    print(var)
-end
