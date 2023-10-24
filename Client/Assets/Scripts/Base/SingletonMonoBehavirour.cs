7f1c16595c8f50bf044585270d98cdaf7143f161
沛大鑫 <peixinhuang0428@gmail.com>
2023-10-24 11:42:14 +0800

    Add single Monobehaviour class

---
+++ Client/Assets/Scripts/Base/SingletonMonoBehavirour.cs
@@ -0,0 +1,19 @@
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    private static T Instance
+    {
+        get{
+            if(_instance == null)
+            {
+                _instance == FindObjectOfType<T>();
+                if(_instance == null)
+                {
+                    GameObject singleObject = new GameObject(typeof(T).Name);
+                    _instance = singleObject.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+}
